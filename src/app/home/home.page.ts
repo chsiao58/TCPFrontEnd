b2293ec2438263d7dadd71b594607f9a0b06eda0
Joanna Hsiao <chsiao58@gmail.com>
2019-07-14 20:30:27 -0400

    can print channel from get request to localhost
    Please install this extention( https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en) before run it on google chrome, required to test localhost

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NavController,NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-
-  constructor() {}
-
+  channels: Observable<any>;
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+    this.channels = this.httpClient.get('http://localhost:8080/channel');
+    this.channels
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
 }
